eb1664b382681c11eb6f28051ad23ae5dca2ab61
devsooki <devsooki@gmail.com>
2022-10-23 23:21:43 +0900

    feat: detail style

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -4,10 +4,18 @@ import styled from 'styled-components';
 
 const Detail = () => {
   const { state } = useLocation()
+  const {
+    title,
+    text,
+    date
+  } = state
+
+  console.log(state)
 
   return (
     <Container>
-      detail
+      <Title>{title}</Title>
+      <Text>{text}</Text>
     </Container>
   )
 }
@@ -16,6 +24,18 @@ export default Detail
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   margin: 50px auto;
   width: 80%;
+`
+const Title = styled.h1`
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  font-size: 20px;
+  border-bottom: 1px solid #ddd;
+`
+const Text = styled.div`
+  font-size: 15px;
+  line-height: 25px;
+  white-space: pre-line
 `
